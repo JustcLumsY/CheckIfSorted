0e3e5d7cb9cdacd2404d464003d4e63e71564e4a
John Espen Thoresen <84008816+JustcLumsY@users.noreply.github.com>
2022-01-05 08:56:06 +0100

    Add project files.

---
+++ CheckIfSorted/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CheckIfSorted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };
+           var result = IsSorted(numberArray);
+            Console.WriteLine(result);
+        }
+
+        public static bool IsSorted(int[] numberArray)
+        {
+            int previousNumber = 0;
+           
+            
+            for (int i = 0; i < numberArray.Length; i++)
+            {
+                int currentNumber = numberArray[i];
+                if (i == 0)
+                {
+                    previousNumber = currentNumber;
+                        continue;
+                }
+                if (previousNumber > currentNumber) return false;
+                previousNumber = currentNumber;
+            }
+            return true;
+
+        }
+    }
+}
